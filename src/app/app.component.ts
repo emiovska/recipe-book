92fe11311cb958bc6f6bb3b5a9cd7b0814339156
Elena Miovska <elena.miovska@musala.com>
2016-11-05 11:05:43 +0200

    -app initialization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'rb-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'rb works!';
